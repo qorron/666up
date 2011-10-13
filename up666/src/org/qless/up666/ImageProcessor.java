6f54fd41e25a0b150272f48af744de3dbcae2342
quattro <github@qless.org>
2011-10-13 22:21:22 +0200

    GPLv3 notice

--- up666/src/org/qless/up666/ImageProcessor.java
+++ up666/src/org/qless/up666/ImageProcessor.java
@@ -1,6 +1,23 @@
-/**
+/*
+ * Copyright 2011 qorron
+ * Contact: https://github.com/qorron
  * 
+ * This file is part of 666up!
+ * 
+ * 666up! is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 666up! is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 666up!.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.qless.up666;
 
 import java.io.DataOutputStream;
@@ -14,7 +31,7 @@ import android.util.Log;
  * 
  * @author quattro
  * 
- * resizes an image if it is too large
+ *         resizes an image if it is too large
  * 
  */
 
@@ -28,10 +45,13 @@ public class ImageProcessor {
 	/**
 	 * loads, resizes and writes the image to the http connection
 	 * 
-	 * @param imagePath the path to the image file
-	 * @param httpPostOutputStream the http stream
+	 * @param imagePath
+	 *            the path to the image file
+	 * @param httpPostOutputStream
+	 *            the http stream
 	 */
-	public static void process(String imagePath, DataOutputStream httpPostOutputStream) {
+	public static void process(String imagePath,
+			DataOutputStream httpPostOutputStream) {
 
 		BitmapFactory.Options options = new BitmapFactory.Options();
 		options.inJustDecodeBounds = true; // to get the size without actually
@@ -47,8 +67,8 @@ public class ImageProcessor {
 
 		int maxPixels = maxPixel * maxPixel;
 		int maxPixelsForLoading = maxPixels; // multiply by 4 to leave some
-													// room and scale down the
-													// rest in memory
+												// room and scale down the
+												// rest in memory
 
 		double baseInSampleSize = 1;
 		Log.i("ImageScale", "maxPixelsForLoading: " + maxPixelsForLoading
@@ -56,9 +76,8 @@ public class ImageProcessor {
 				+ baseInSampleSize);
 
 		for (; originalPixels > maxPixelsForLoading; originalPixels /= 4) {
-			baseInSampleSize *=2;
+			baseInSampleSize *= 2;
 		}
-
 
 		// outMimeType
 
@@ -74,13 +93,13 @@ public class ImageProcessor {
 		int width = bitmapOrg.getWidth();
 		int height = bitmapOrg.getHeight();
 
-		// calculate the scale 
+		// calculate the scale
 		float scaleWidth = 1;
 		float scaleHeight = 1;
 		scaleWidth = scaleHeight = ((float) maxPixels) / originalPixels;
-		
-		Log.i("ImageScale", "width: " + width + " height: "
-				+ height + "scale: " + scaleWidth);
+
+		Log.i("ImageScale", "width: " + width + " height: " + height
+				+ "scale: " + scaleWidth);
 
 		// create a matrix for the manipulation
 		Matrix matrix = new Matrix();
@@ -89,14 +108,15 @@ public class ImageProcessor {
 		// rotate the Bitmap
 		// matrix.postRotate(45);
 
-		bitmapOrg.compress(Bitmap.CompressFormat.JPEG, 90,httpPostOutputStream);
+		bitmapOrg
+				.compress(Bitmap.CompressFormat.JPEG, 90, httpPostOutputStream);
 
 		// recreate the new Bitmap
-//		Bitmap resizedBitmap = Bitmap.createBitmap(bitmapOrg, 0, 0, width,
-//				height, matrix, true);
-//
-//		resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 90,
-//				httpPostOutputStream);
+		// Bitmap resizedBitmap = Bitmap.createBitmap(bitmapOrg, 0, 0, width,
+		// height, matrix, true);
+		//
+		// resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 90,
+		// httpPostOutputStream);
 
 	}
 }
--- up666/src/org/qless/up666/Up666Activity.java
+++ up666/src/org/qless/up666/Up666Activity.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2011 qorron
+ * Contact: https://github.com/qorron
+ * 
+ * This file is part of 666up!
+ * 
+ * 666up! is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 666up! is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 666up!.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.qless.up666;
 
 import android.app.Activity;
